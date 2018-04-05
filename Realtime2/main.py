661ca5de69b4a56058ae9decd44dbacdaf1a8458
Francisco Prin <franciscoprin@gmail.com>
2018-04-05 09:47:37 -0500

    Adding consumer inside of the librery real time.

---
+++ Realtime2/main.py
@@ -0,0 +1,19 @@
+from GraphThread import GraphThread
+from Queue import Queue
+from ConsumerThread import ConsumerThread
+import threading
+
+points = Queue()
+semaphore = threading.Semaphore(1)
+consumer = ConsumerThread(1,points,semaphore)
+graph = GraphThread(0.5,points,semaphore)
+
+
+# Start will immediately call the run immediately inside of ProducerThread and ConsumerThread classes..
+consumer.start()
+# graph.start()
+consumer.join()
+# graph.join()
+
+print ("Exiting Main Thread")
+
